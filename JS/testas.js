ccc2f8ef570fd33a5735e0e0ec46ea8d4b863ea2
Slashass <70883938+Slashass@users.noreply.github.com>
2020-09-28 11:03:44 +0300

    js testas

---
+++ JS/testas.js
@@ -0,0 +1,34 @@
+function autoAshys(ratai) {
+    //return ratai / 2;
+    if (ratai === 4) {
+        return 2;
+    } else {
+        return (ratai - 2) / 4 + 1;
+    }
+}
+
+console.log(autoAshys(4))
+console.log(autoAshys(10))
+console.log(autoAshys(14))
+
+console.log('-----------------------------------')
+
+const appSize = [4, 8, 7, 6, 1, 1, 9, 45, 7, 2];
+const phoneMemorySize = 70;
+
+function kiekTilpsAppsu(memory, list) {
+    let usedSpace = 0;
+    const appCount = list.length;
+
+    for (let i=0; i<appCount; i++) {
+        const appSizee = list[i];
+        usedSpace += appSizee;
+        console.log(appSizee, usedSpace);
+    }
+
+    return 0;
+}
+
+const phoneAppCount = kiekTilpsAppsu(phoneMemorySize, appSize);
+
+console.log(phoneAppCount);
